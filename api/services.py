43e04de6d205945ab87bb3cca305dfa4f909a75f
Mikel Losada <mikel.losada@gmail.com>
2020-07-16 12:36:30 +0200

    Refactor services ELO functions and add code to update ratings

--- api/services.py
+++ api/services.py
@@ -4,8 +4,18 @@ import uuid
 import chess
 from django.contrib.auth import get_user_model
 from django.utils import timezone
+from django.core.exceptions import ObjectDoesNotExist
 
-from .models import Board, Game, Move, Result
+from .models import Board, Game, Move, Result, Elo
+
+
+RESULTS_DICT = {
+    "1-0": Result.WHITE_WINS,
+    "1/2-1/2": Result.DRAW,
+    "0-1": Result.BLACK_WINS,
+}
+
+User = get_user_model()
 
 
 def is_game_over(game_instance):
@@ -13,25 +23,24 @@ def is_game_over(game_instance):
     Update the game if it is over
     Return True if the game is over, False if it is not
     """
-    results_dict = {
-        "1-0": Result.WHITE_WINS,
-        "1/2-1/2": Result.DRAW,
-        "0-1": Result.BLACK_WINS,
-    }
 
-    board = chess_board_from_uuid(game_instance.uuid)
+    chess_board = chess_board_from_uuid(game_instance.uuid)
 
-    if board.is_game_over():
-        result_string = board.result()
-        game_instance.result = Result(
-            result=results_dict.get(result_string), termination=Result.NORMAL
-        )
-        game_instance.finished_at = timezone.now()
-        game_instance.result.save()
-        game_instance.save()
+    if chess_board.is_game_over():
+        finish_game(game_instance, chess_board)
         return True
 
     return False
+
+
+def finish_game(game_instance, chess_board):
+    result_string = chess_board.result()
+    game_instance.result = Result(
+        result=RESULTS_DICT.get(result_string), termination=Result.NORMAL
+    )
+    game_instance.finished_at = timezone.now()
+    game_instance.result.save()
+    game_instance.save()
 
 
 def assign_color(game_instance, username, preferred_color="white"):
@@ -49,7 +58,6 @@ def assign_color(game_instance, username, preferred_color="white"):
         if preferred_color == "random":
             player_color = "black" if random.randint(0, 1) == 1 else "white"
 
-    User = get_user_model()
     auth_user = User.objects.get(username=username)
 
     if player_color == "black":
@@ -126,10 +134,12 @@ def create_board_from_pgn(pgn_file, starting_at=0):
         board_instance.save()
 
         if starting_at:
-            move_ucis = [i.move.uci() for i in chess_game.mainline()][:starting_at]
+            move_ucis = [i.move.uci()
+                         for i in chess_game.mainline()][:starting_at]
 
             for u in move_ucis:
-                move_piece(board_instance, u[:2], u[2:], chess_board=chess_board)
+                move_piece(board_instance, u[:2],
+                           u[2:], chess_board=chess_board)
 
     return (board_instance, chess_board)
 
@@ -137,26 +147,80 @@ def create_board_from_pgn(pgn_file, starting_at=0):
 # ELO
 
 
-def get_expected_score(elo_instance, opponent_rating):
+def get_expected_score(player_rating, opponent_rating):
     """
     https://wikimedia.org/api/rest_v1/media/math/render/svg/51346e1c65f857c0025647173ae48ddac904adcb
     """
 
-    return 1 / (1 + 10 ** ((opponent_rating - elo_instance.rating) / 400))
+    return 1 / (1 + 10 ** ((opponent_rating - player_rating) / 400))
 
 
-def update_rating(elo_instance, score, opponent_rating):
+def update_elo_rating(player_score=None, player=None, opponent=None):
     """
     https://wikimedia.org/api/rest_v1/media/math/render/svg/09a11111b433582eccbb22c740486264549d1129
 
-    Update rating after a single game (score should be either 0, 0.5 or 1)
+    Update a player's rating after a game (score should be either 0, 0.5 or 1)
     """
 
-    expected_score = elo_instance.get_expected_score(opponent_rating)
-    elo_instance.rating = round(
-        elo_instance.rating + elo_instance.k_factor * (score - expected_score)
-    )
-    elo_instance.save()
+    if player_score and isinstance(player, User) and isinstance(opponent, User):
+        player_elo = player.elo
+        opponent_elo = opponent.elo
+
+        expected_score = get_expected_score(
+            player_elo.rating, opponent_elo.rating)
+
+        player_elo.rating = round(
+            player_elo.rating + player_elo.k_factor *
+            (player_score - expected_score)
+        )
+
+        player_elo.save()
+
+
+def update_elo(game_instance):
+    """
+    Update the ELO of a game's players according to the game's result
+    Returns: white_elo, black_elo
+    """
+
+    white = game_instance.whites_player
+    black = game_instance.blacks_player
+
+    scores = {
+        "white": 0,
+        "black": 0,
+    }
+
+    result = game_instance.result
+
+    if result == Result.DRAW:
+        scores["white"] += 0.5
+        scores["black"] += 0.5
+        white.elo.draws += 1
+        black.elo.draws += 1
+
+    elif result == Result.WHITE_WINS:
+        scores["white"] += 1
+        white.elo.wins += 1
+        black.elo.losses += 1
+
+    elif result == Result.BLACK_WINS:
+        scores["black"] += 1
+        black.elo.wins += 1
+        white.elo.losses += 1
+
+    white.elo.save()
+    black.elo.save()
+
+    update_elo_rating(
+        player_score=scores["white"], player=white, opponent=black)
+    update_elo_rating(
+        player_score=scores["black"], player=black, opponent=white)
+
+    return white.elo, black.elo
+
+
+# GAME
 
 
 def create_game(result_data=None, board_data=None, **validated_data):
