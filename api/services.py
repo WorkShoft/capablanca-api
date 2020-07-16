7613bf4d891c5da8fb554d5a828eccc7b97a3ea6
Mikel Losada <mikel.losada@gmail.com>
2020-07-16 17:20:21 +0200

    Write functionality to update both player's ELO after a game

--- api/services.py
+++ api/services.py
@@ -4,10 +4,9 @@ import uuid
 import chess
 from django.contrib.auth import get_user_model
 from django.utils import timezone
-from django.core.exceptions import ObjectDoesNotExist
 
-from .models import Board, Game, Move, Result, Elo
-
+from .constants import K_FACTOR
+from .models import Board, Game, Move, Result
 
 RESULTS_DICT = {
     "1-0": Result.WHITE_WINS,
@@ -134,12 +133,10 @@ def create_board_from_pgn(pgn_file, starting_at=0):
         board_instance.save()
 
         if starting_at:
-            move_ucis = [i.move.uci()
-                         for i in chess_game.mainline()][:starting_at]
+            move_ucis = [i.move.uci() for i in chess_game.mainline()][:starting_at]
 
             for u in move_ucis:
-                move_piece(board_instance, u[:2],
-                           u[2:], chess_board=chess_board)
+                move_piece(board_instance, u[:2], u[2:], chess_board=chess_board)
 
     return (board_instance, chess_board)
 
@@ -147,34 +144,52 @@ def create_board_from_pgn(pgn_file, starting_at=0):
 # ELO
 
 
-def get_expected_score(player_rating, opponent_rating):
+def _get_expected_score(player_rating, opponent_rating):
     """
     https://wikimedia.org/api/rest_v1/media/math/render/svg/51346e1c65f857c0025647173ae48ddac904adcb
+
+    Returns the expected score rounded to two decimals
+
+    For instance, a player with 100 less ELO points than their opponent has an expected score
+    of 0.35993500019711494, which is rounded to 0.36 (a 36% 'chance' of winning)
     """
 
-    return 1 / (1 + 10 ** ((opponent_rating - player_rating) / 400))
+    expected_score = 1 / (1 + 10 ** ((opponent_rating - player_rating) / 400))
+
+    return round(expected_score, 2)
 
 
-def update_elo_rating(player_score=None, player=None, opponent=None):
+def get_rating(player_score, player_rating, opponent_rating):
+    expected_score = _get_expected_score(player_rating, opponent_rating)
+
+    return round(player_rating + K_FACTOR * (player_score - expected_score))
+
+
+def update_elo_rating(player_score=None, player=None, opponent=None, previous=False):
     """
     https://wikimedia.org/api/rest_v1/media/math/render/svg/09a11111b433582eccbb22c740486264549d1129
 
-    Update a player's rating after a game (score should be either 0, 0.5 or 1)
+    Update a player's rating after a game 
+
+    score: [0 | 0.5 | 1]
+
+    The second player to be updated should set previous=True. When previous is True
+    the ELO will be calculated based on their opponent's ELO before the game
     """
 
-    if player_score and isinstance(player, User) and isinstance(opponent, User):
-        player_elo = player.elo
-        opponent_elo = opponent.elo
-
-        expected_score = get_expected_score(
-            player_elo.rating, opponent_elo.rating)
-
-        player_elo.rating = round(
-            player_elo.rating + player_elo.k_factor *
-            (player_score - expected_score)
+    if (
+        player_score is not None
+        and isinstance(player, User)
+        and isinstance(opponent, User)
+    ):
+        opponent_rating = (
+            opponent.elo.rating if previous is False else opponent.elo.previous_rating
         )
+        new_rating = get_rating(player_score, player.elo.rating, opponent_rating)
 
-        player_elo.save()
+        player.elo.update_rating(new_rating)
+
+    return player.elo.rating
 
 
 def update_elo(game_instance):
@@ -191,7 +206,7 @@ def update_elo(game_instance):
         "black": 0,
     }
 
-    result = game_instance.result
+    result = game_instance.result.result
 
     if result == Result.DRAW:
         scores["white"] += 0.5
@@ -212,10 +227,11 @@ def update_elo(game_instance):
     white.elo.save()
     black.elo.save()
 
+    update_elo_rating(player_score=scores["white"], player=white, opponent=black)
+
     update_elo_rating(
-        player_score=scores["white"], player=white, opponent=black)
-    update_elo_rating(
-        player_score=scores["black"], player=black, opponent=white)
+        player_score=scores["black"], player=black, opponent=white, previous=True
+    )
 
     return white.elo, black.elo
 
