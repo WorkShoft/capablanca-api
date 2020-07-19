8b6133a4a14cbab414178a18037577f84678ebe8
Mikel Losada <mikel.losada@gmail.com>
2020-07-19 11:41:33 +0200

    Add JWT_TOKEN, CHESS_BOARD, BOARD_INSTANCE and game_instance fixtures

--- api/tests/fixtures.py
+++ api/tests/fixtures.py
@@ -1,6 +1,17 @@
 import pytest
+import chess
 
 from api import services
+from api.models import Board, Game, Result
+
+
+JWT_TOKEN = {
+    "refresh": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTU5NTIzMjMxMCwianRpIjoiMTViM2ZiZGNhODJlNDBiMDkyNTBiYzA5ZTlkODQwMmYiLCJ1c2VyX2lkIjoxfQ.3UZIhcS4X14zb9V7wRnf0G3TJ1f7G6UMijThokvOD_M",
+    "access": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNTk1MTQ1OTIyLCJqdGkiOiI1ODBjNmM3ZDcyNTk0NmJmOWJiNGY1YmMyMzcyMjY0MiIsInVzZXJfaWQiOjF9.44RKjIbKXmWqjzJA0TtpbnBTt3-3tAMxUP1EZDMJais",
+}
+
+CHESS_BOARD = chess.Board()
+BOARD_INSTANCE = Board.from_fen(CHESS_BOARD.fen())
 
 
 @pytest.fixture
@@ -22,3 +33,15 @@ def users():
     )
 
     return user_one, user_two
+
+
+@pytest.fixture
+def game_instance(users):
+    player, opponent = users
+
+    return Game(
+        board=BOARD_INSTANCE,
+        whites_player=player,
+        blacks_player=opponent,
+        result=Result(result=Result.BLACK_WINS, termination=Result.NORMAL),
+    )
