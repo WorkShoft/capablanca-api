fe68b24efd5c8a3248f44b92d2e99a369c1d661d
Mikel Losada <mikel.losada@gmail.com>
2020-07-19 12:46:36 +0200

    Add unit tests for the board __str__ and move_stack property

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -1,4 +1,6 @@
+import pytest
 import chess
+
 from api.models import Board
 
 
@@ -11,3 +13,16 @@ def test_board_from_fen():
     assert board.halfmove_clock == 0
     assert board.fullmove_number == 1
     assert board.castling_xfen == "KQkq"
+
+
+@pytest.mark.django_db
+def test_get_empty_move_stack():
+    board = Board()
+
+    assert board.move_stack == []
+
+
+@pytest.mark.django_db
+def test_board_model_string():
+    board = Board()
+    assert str(board) == board.fen
