81d3bb883db2054aa1b95666cf3d2e30bb1108e5
Mikel Losada <mikel.losada@gmail.com>
2020-07-21 20:13:56 +0200

    Make GameConsumer an AsyncWebsocketConsumer

--- stream_app/consumers.py
+++ stream_app/consumers.py
@@ -1,47 +1,43 @@
 import json
 
 from api.models import Game
-from api.serializers import GameSerializer
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+from stream_app.services import get_serialized_game
 
 
-class GameConsumer(WebsocketConsumer):
-    serializer_class = GameSerializer
-
-    def connect(self):
+class GameConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.uuid = self.scope["url_route"]["kwargs"]["uuid"]
         self.game_group_name = f"game_{self.uuid}"
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.game_group_name, self.channel_name
         )
+    
+        await self.accept()
 
-        self.accept()
-
-    def receive(self, text_data):
+    async def receive(self, text_data):
         data_json = json.loads(text_data)
-
+        
         if "update" in data_json:
-            game = self.get_serialized_game()
+            game = await get_serialized_game(data_json.get("uuid"))
 
             # Update both players' game data
-            async_to_sync(self.channel_layer.group_send)(
-                self.game_group_name, {"type": "game_data", "game": game,}
+            await self.channel_layer.group_send(
+                self.game_group_name, {"type": "game_data", "game": game, }
             )
-
-    def game_data(self, data):
+        
+    async def game_data(self, data):
         game = data["game"]
 
         # Send game over WebSocket
-        self.send(text_data=json.dumps(game))
+        await self.send(text_data=json.dumps(game))
 
-    def disconnect(self, *args, **kwargs):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, *args, **kwargs):
+        await self.channel_layer.group_discard(
             self.game_group_name, self.channel_name
         )
-
-    def get_serialized_game(self):
-        game = Game.objects.get(uuid=self.uuid)
-        return self.serializer_class(game).data
+    
